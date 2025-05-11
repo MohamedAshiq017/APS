46e5a8c3285095953db9250369bb60f228e855d3
Ashiq <mohamedashiq120404@gmail.com>
2025-05-11 22:35:23 +0530

    api url path fix

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -20,7 +20,7 @@ const AdminContextProvider = (props) =>{
 
     const getAllDoctors = async() =>{
 try{
-    const {data} = await axios.post(backendUrl + 'api/admin/all-doctors',{},{headers:{aToken}}) 
+    const {data} = await axios.post(backendUrl + '/api/admin/all-doctors',{},{headers:{aToken}}) 
     if(data.success){
         setDoctors(data.doctors)
         console.log(data.doctors)
